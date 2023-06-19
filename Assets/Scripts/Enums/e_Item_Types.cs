48e35899ab479090e3e519dfc9b0e88a77e330a0
Alp Gorusuk <malpgorusuk@gmail.com>
2023-06-19 20:08:35 +0300

    -Edit Images & -Create Scene earlyver01_1

---
+++ Assets/Scripts/Enums/e_Item_Types.cs
@@ -0,0 +1,10 @@
+public enum e_Item_Types
+{
+    None,
+    Skin,
+    Point,
+    Chest,
+    Coin,
+    Money,
+    Bomb
+}
---
+++ Assets/Scripts/Enums/e_Spin_Direction.cs
@@ -0,0 +1,5 @@
+public enum e_Spin_Direction
+{
+    Clockwise,
+    CounterClockwise
+}
